dd0456b0f0dd7a606d46706b3bb4b17e83eaf278
euclides981 <euclides981@gmail.com>
2022-05-20 16:26:39 -0300

    imgs

--- js/script.js
+++ js/script.js
@@ -121,18 +121,16 @@ function checarJogo() {
         informa.innerHTML = `Que pena...<br>Não acertou a palavra secreta:
         <br>${palavraSorteada}<br>0${letrasErradas.length} palpites errados
         <br>0${letrasCorretas.length} palpites corretos<br>
-        Total de tentativas: ${letrasCorretas.length + letrasErradas.length}
-        <br> Jogue Novamente.`
+        Total de tentativas: ${letrasCorretas.length + letrasErradas.length}`
     }
     if(palavraSorteada == secreta.innerHTML) {
         document.getElementById('trocaImg').src="img/forcaV.svg"
         document.querySelector('.esconde').style.display = 'none'
         document.querySelector('#resultado').style.display = 'block'
         informa.innerHTML = `Parabéns...<br>Você acertou a palavra secreta:
-        <br>${palavraSorteada}<br>Teve: ${letrasErradas.length} chutes errados.
-        <br>Teve: ${letrasCorretas.length} chutes corretos<br>
-        Total de tentativas: ${letrasCorretas.length + letrasErradas.length}
-        <br> Jogue Novamente.`
+        <br>${palavraSorteada}<br>0${letrasErradas.length} palpites errados.
+        <br>0${letrasCorretas.length} palpites corretos<br>
+        Total de tentativas: ${letrasCorretas.length + letrasErradas.length}`
     }
 }
 desenharForca()
